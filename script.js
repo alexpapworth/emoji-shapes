f2e0bbbe531c4218194a1a4b02b3fe3103fc77f5
Alex Papworth <arpapworth@gmail.com>
2019-04-05 13:28:03 +0100

    Add Emoji replacement

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 var ready;
 
-star = "‪                   😀‬\n‪                😀 😀‬\n‪             😀       😀‬\n😀😀😀             😀😀😀‬\n‪    😀                           😀‬\n‪        😀       😀      😀‬\n‪      😀     😀   😀    😀‬\n‪    😀   😀           😀  😀‬\n‪  😀 😀                   😀😀‬";
-boot = "😃 😃 😃 😃\n 😃             😃\n 😃            😃\n 😃            😃\n 😃            😃\n 😃            😃\n 😃            😃\n 😃              😃\n😃                    😃  \n😃                          😃  😃     \n  😃                                       😃\n     😃😃😃😃😃😃😃😃";
+star = "‪                   🤠‬\n‪                🤠 🤠‬\n‪             🤠       🤠‬\n🤠🤠🤠             🤠🤠🤠‬\n‪    🤠                           🤠‬\n‪        🤠       🤠      🤠‬\n‪      🤠     🤠   🤠    🤠‬\n‪    🤠   🤠           🤠  🤠‬\n‪  🤠 🤠                   🤠🤠‬";
+boot = "🤠 🤠 🤠 🤠\n 🤠             🤠\n 🤠            🤠\n 🤠            🤠\n 🤠            🤠\n 🤠            🤠\n 🤠            🤠\n 🤠              🤠\n🤠                    🤠  \n🤠                          🤠  🤠     \n  🤠                                       🤠\n     🤠🤠🤠🤠🤠🤠🤠🤠";
 heart = "       🤠         🤠\n   🤠      🤠      🤠\n🤠                        🤠\n🤠                        🤠\n  🤠                    🤠\n     🤠              🤠\n          🤠     🤠\n               🤠";
 
 function setShape() {
-
 	emoji = document.querySelector('#input').value;
-	document.querySelector('#output').value = star;
+
+	if (document.querySelector('input[name="shape"]:checked'))
+	{
+		shape = document.querySelector('input[name="shape"]:checked').value
+
+		newShape = window[shape].replace(/🤠/g, emoji);
+
+		document.querySelector('#output').value = newShape;
+	}
 }
 
 ready = function() {
 
 	document.querySelector('#input').addEventListener('input', setShape)
-
+	for (x = 0; x < document.querySelectorAll('input[name="shape"]').length; x++)
+	{
+		document.querySelectorAll('input[name="shape"]')[x].addEventListener('click', setShape)
+	}
 }
 
 document.addEventListener('DOMContentLoaded', ready);
